42a252faa531a5bf1aacf3691b97306970f61ace
Josh Miklos <simulatorleek@gmail.com>
2024-08-03 18:58:34 -0700

    pyr conv compiles and doesn't work

--- sili/modules/pyramid_conv_d2d/pyr_conv.py
+++ sili/modules/pyramid_conv_d2d/pyr_conv.py
@@ -1,3 +1,5 @@
+import pickle
+
 import kp
 import numpy as np
 from displayarray import DirectDisplay
@@ -5,12 +7,16 @@ import cv2
 import os
 
 from sili.buffers.pyramid import ImagePyramidBuffer, calculate_pyramid_levels
-from sili.buffers.image import ImageBuffer, GrayImageBuffer
+from sili.buffers.image import ImageBuffer, ndarrayBuffer
 from sili.core.devices.gpu import GPUManager, get_shader
 from sili.core.module import Module
+from sili.core.util import find_good_dimension_sizes
 from typing import Union
 
 file_path = os.path.dirname(os.path.abspath(__file__))
+
+# todo: save an int8 pyramid like is expected and input that
+# todo: remove the  indexing optimization if that doesn't help
 
 class PyrConv(Module):
     def __init__(self,
@@ -26,76 +32,40 @@ class PyrConv(Module):
         self.forward_shader = get_shader(file_path + os.sep + 'conv_pyr_forward.comp')
 
         assert isinstance(image_pyr, ImagePyramidBuffer)
-        self.in_img_pyr = ImagePyramidBuffer(self.gpu, image_pyr)
+        self.in_img_pyr = ImagePyramidBuffer(self.gpu, image_pyr.levels, image_pyr.channels, image_pyr.use_lvl_buf,
+                                             type=image_pyr.type if hasattr(image_pyr, 'tyoe') else np.float32  # todo: update image_pyr to always save type
+                                             )
         self.out_pyr = ImagePyramidBuffer(gpu, self.in_img_pyr.levels, type=np.float32)
 
         if callable(init_conv):
-            self.conv = GrayImageBuffer(gpu, init_conv(3))
+            self.conv = ndarrayBuffer(gpu, init_conv(3, 3, 3, 3, 3))
         elif isinstance(init_conv, np.ndarray):
-            self.conv = GrayImageBuffer(gpu, init_conv)
+            self.conv = ndarrayBuffer(gpu, init_conv)
         else:
-            self.conv = GrayImageBuffer(gpu, np.zeros((3, 3)))
-        self.depth_conv_str = GrayImageBuffer(gpu, np.zeros([self.conv.width, self.conv.height]))  # adagrad weights
+            self.conv = ndarrayBuffer(gpu, np.ones((3, 3, 3, 3, 3)))
 
+        self.conv_str = ndarrayBuffer(gpu, np.zeros(self.conv.shape))  # adagrad weights
 
         # PIPELINE OBJECTS:
-        # these need to be accessible so that kompute can record input/output for pipelines
-        self.forward_input_buffers = [self.image.buffer, self.out_pyr.pyr_lvl_buffer]
+        self.forward_input_buffers = [self.conv.buffer, self.in_img_pyr.image_buffer, self.in_img_pyr.pyr_lvl_buffer]
         self.forward_output_buffers = [self.out_pyr.image_buffer]
-        self.internal_buffers = [self.level_count_buffer]
 
-        self.clear_algorithm = self.gpu.manager.algorithm(
-            [*self.forward_output_buffers],
-            spirv=self.forward_shader_clear,
-            workgroup=[int(np.ceil(self.out_pyr.size / (self.gpu.max_workgroup_invocations))), 0, 0],
-            spec_consts=np.asarray([self.gpu.max_workgroup_invocations, self.out_pyr.size], dtype=np.uint32).view(
-                np.float32)
+        #chunk_w, chunk_h = find_good_dimension_sizes(self.gpu.max_workgroup_invocations, 2)
+        chunk_w, chunk_h = 8, 8
+
+        self.forward_algorithm = self.gpu.manager.algorithm(
+            [*self.forward_input_buffers, *self.forward_output_buffers],
+            spirv=self.forward_shader,
+            workgroup=[int(np.ceil(self.in_img_pyr.size / (self.in_img_pyr.channels * self.gpu.max_workgroup_invocations))), 0, 0],
+            spec_consts=np.asarray([
+                self.gpu.max_workgroup_invocations,
+                3,3,3,3,
+                chunk_w, chunk_h
+            ], dtype=np.uint32).view(np.float32)
         )
-        self.count_algorithm = self.gpu.manager.algorithm(
-            [*self.internal_buffers],
-            spirv=self.count_shader,
-            workgroup=[1, 0, 0]
-        )
-
-        self.split_point = self.compute_split_point()
-        self.forward_algorithms_large = []
-        for i in reversed(range(self.split_point, self.out_pyr.levels[1])):
-            self.forward_algorithms_large.append(self.gpu.manager.algorithm(
-                [*self.forward_input_buffers, *self.forward_output_buffers, *self.internal_buffers],
-                spirv=self.forward_shader_large,
-                workgroup=[int(np.ceil(self.out_pyr.levels[i*3+2+1]*self.out_pyr.levels[i*3+2+2]*self.out_pyr.channels/(self.gpu.max_workgroup_invocations*4))), 0,
-                           0],
-                spec_consts=np.asarray([self.gpu.max_workgroup_invocations], dtype=np.uint32).view(np.float32)
-            ))
-
-        j = self.split_point-1
-        self.forward_algorithm_small = self.gpu.manager.algorithm(
-            [*self.forward_input_buffers, *self.forward_output_buffers, *self.internal_buffers],
-            spirv=self.forward_shader_small,
-            workgroup=[int(np.ceil(self.out_pyr.levels[j*3+2+1]*self.out_pyr.levels[j*3+2+2]*self.out_pyr.channels/(self.gpu.max_workgroup_invocations*4))), 0, 0],
-            spec_consts=np.asarray([self.gpu.max_workgroup_invocations], dtype=np.uint32).view(np.float32)
-        )
-
-        self.forward_input_buffers.extend(self.internal_buffers)  # to work with regular runners
-
-    def compute_split_point(self):
-        for l in range(2, len(self.out_pyr.levels), 3):
-            start, width, height = int(self.out_pyr.levels[l]), int(self.out_pyr.levels[l + 1]), int(self.out_pyr.levels[l + 2]),
-            end_idx = int(start * self.out_pyr.channels + (width * height * self.out_pyr.channels))
-
-            if end_idx > self.gpu.max_workgroup_invocations:
-                return int((l-2)/3)
-
-        return self.out_pyr.levels[1]  # the actual number of levels is stored here
 
     def forward_ops(self):
-        ops = [kp.OpAlgoDispatch(self.clear_algorithm)]
-        j=0
-        for _ in range(self.split_point, self.out_pyr.levels[1]):
-            ops.append(kp.OpAlgoDispatch(self.forward_algorithms_large[j]))
-            ops.append(kp.OpAlgoDispatch(self.count_algorithm))
-            j+=1
-        ops.append(kp.OpAlgoDispatch(self.forward_algorithm_small))
+        ops = [kp.OpAlgoDispatch(self.forward_algorithm)]
         return ops
 
     def backward_ops(self):
@@ -105,17 +75,23 @@ class PyrConv(Module):
         return []
 
 
+def image_normalize(im:np.ndarray):
+    max_val = np.max(im)
+    min_val = np.min(im)
+    span = max_val-min_val
+    im2 = (im/span)-min_val  # now 0 to 1
+    return im2
 
-def display_basic_forward_sequence(pyr, seq, image, display=None):
+def display_basic_forward_sequence(pyr, seq, pyr_in:ImagePyramidBuffer, display=None):
     if display is None:
         display = DirectDisplay()
 
-    pyr.image.set(image)
+    pyr.in_img_pyr.set(pyr_in.image_buffer)
     seq.eval()
     out_images = pyr.out_pyr.get()
 
     for i, o in enumerate(out_images):
-        display.imshow(f'output {i}', o)
+        display.imshow(f'output {i}', image_normalize(o))
     while True:
         display.update()
         if display.window.is_closing:
@@ -140,14 +116,15 @@ def generate_pyramid_file(input_image_file, output_pyr_file):
         print(im_pyr)
 
 
-def display_pyramid(input_image_file):
+def display_pyramid(input_pyr_file):
     from sili.core.runners import get_forward_sequence
 
     gpu = GPUManager()
-    im = cv2.imread(input_image_file).astype(np.uint8)
-    pyr = ToImagePyramid(gpu, im)  # im used to set width, height, and channels
+    with open(input_pyr_file, mode='rb') as f:
+        pyr_in = pickle.load(f)
+    pyr = PyrConv(gpu, pyr_in)  # im used to set width, height, and channels
     seq = get_forward_sequence([pyr])
-    display_basic_forward_sequence(pyr, seq, im)
+    display_basic_forward_sequence(pyr, seq, pyr_in)
 
 
 def display_pyramid_from_camera(camera):
@@ -184,9 +161,9 @@ def display_pyramid_from_camera(camera):
             break
 
 if __name__ == '__main__':
-    #display_pyramid("../../../test/files/test_ai_pls_ignore.png")
+    display_pyramid("../../../test/files/test_ai_pyr_pls_ignore.pyr")
 
     # generate_pyramid_file("../../../test/files/test_ai_pls_ignore.png",
     #                      "../../../test/files/test_ai_pyr_pls_ignore.pyr")
 
-    display_pyramid_from_camera(0)
+    #display_pyramid_from_camera(0)
